e6cb4c532c314807134151cda9ed0528080c42bf
Wauzmons <waus@web.de>
2019-07-01 18:02:23 +0200

    Wazera Kanban Test 1

---
+++ Wazera/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Wazera
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenKanbanBoard(object sender, RoutedEventArgs e)
+        {
+            Kanban kanban = new Kanban();
+            kanban.Show();
+        }
+    }
+}
