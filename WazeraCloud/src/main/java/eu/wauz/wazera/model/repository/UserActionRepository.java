4950ac1464480a4c4174ab83f320f83ba95b0753
Wauzmons <dev@wauz.eu>
2019-11-06 19:35:19 +0100

    Cleanup

--- WazeraCloud/src/main/java/eu/wauz/wazera/model/repository/UserActionRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.wauz.wazera.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import eu.wauz.wazera.model.entity.UserAction;
-
-public interface UserActionRepository extends CrudRepository<UserAction, Integer> {
-
-}
--- WazeraCloud/src/main/java/eu/wauz/wazera/model/repository/jpa/FolderUserDataJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.wauz.wazera.model.repository.jpa;
-
-import eu.wauz.wazera.model.entity.FolderUserData;
-
-public interface FolderUserDataJpaRepository {
-
-	FolderUserData findByFolderAndUser(Integer folderId, String userName);
-
-}
